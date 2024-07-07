245c4ab5d9b11459f07c1fcef4f543ed9f32da00
IlyasBen <73348981+ilyasben26@users.noreply.github.com>
2024-07-07 16:16:03 +0200

    added nuclei logs

--- core/config.py
+++ core/config.py
@@ -1,13 +1,19 @@
-DB_PATH = "./ilyas-recon.sqlite3"
-BACKUP_DIR_PATH = "./backup"
-
-SUBLIST3R_TEMP = "temp/sublist3r_temp.txt"
-AMASS_TEMP = "temp/amass_temp.txt"
-SUBFINDER_TEMP = "temp/subfinder_temp.txt"
+# Edit these with your own locations
 SUBLIST3R_PATH = "/Users/ilyas/Documents/GitHub/My-Recon-Methodology/Sublist3r/sublist3r.py"
 SUBFINDER_PATH = "$HOME/go/bin/subfinder"
 OAM_SUBS_PATH = "$HOME/go/bin/oam_subs"
 
+# Edit or keep, you choose (just leave it)
+RESOLVERS = "inputs/resolvers.txt"
+
+# DO NOT TOUCH
+DB_PATH = "./ilyas-recon.sqlite3"
+BACKUP_DIR_PATH = "./backup"
+SUBLIST3R_TEMP = "temp/sublist3r_temp.txt"
+AMASS_TEMP = "temp/amass_temp.txt"
+SUBFINDER_TEMP = "temp/subfinder_temp.txt"
 MASSDNS_INPUT_TEMP = "temp/massdns_input_temp.txt"
 MASSDNS_OUTPUT_TEMP = "temp/massdns_output_temp.txt"
-RESOLVERS = "inputs/resolvers.txt"
+NUCLEI_LOGS = "logs/nuclei_logs.txt"
+
+
--- core/nuclei.py
+++ core/nuclei.py
@@ -1,9 +1,14 @@
 from core.db import update_tags
+import core.config as config
 import re
 from typing import List, Tuple
+import os
 
 def integrate_nuclei_results(nuclei_results_file: str, db_path: str) -> None:
     """Integrate nuclei scan results into the database."""
+    ##TODO: save it into the log file
+    append_to_nuclei_logs(nuclei_results_file, config.NUCLEI_LOGS)
+    # Saving it into the database
     parsed_results = parse_nuclei_results(nuclei_results_file)
     update_tags(parsed_results, db_path)
 
@@ -23,3 +28,21 @@ def parse_nuclei_results(filename: str) -> List[Tuple[str, str]]:
                 result.append((domain, tags))
     
     return result
+
+def append_to_nuclei_logs(nuclei_results_file: str, log_file: str) -> None:
+    """Append raw nuclei scan results to the nuclei logs, avoiding duplicates."""
+    existing_lines = set()
+    
+    # Read existing entries to avoid duplicates
+    if os.path.exists(log_file):
+        with open(log_file, 'r') as f:
+            for line in f:
+                existing_lines.add(line.strip())
+    
+    # Append new entries that are not duplicates
+    with open(log_file, 'a') as f:
+        with open(nuclei_results_file, 'r') as results:
+            for line in results:
+                if line.strip() not in existing_lines:
+                    f.write(f"{line}")
+                    existing_lines.add(line.strip())  # Update set to avoid duplicates in memory
