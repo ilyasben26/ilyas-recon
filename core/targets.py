4ad843459654bf8b1007f9ca41a9acc60fab2645
IlyasBen <73348981+ilyasben26@users.noreply.github.com>
2024-07-08 11:23:36 +0200

    added more input verification on subdomain import

--- core/targets.py
+++ core/targets.py
@@ -2,15 +2,30 @@ from core.db import insert_targets
 from termcolor import colored  # type: ignore
 from typing import List
 import core.logging as logging
+import re
 
 
 def integrate_targets(filename: str, db_path: str) -> None:
     """Integrate targets from a file into the database."""
-    domain_list: List[str] = []
-    with open(filename, 'r') as file:
-        domain_list = [line.strip() for line in file]
-    
+    domain_list: List[str] = extract_subdomains(filename)
     insert_targets(domain_list, db_path)
+
+def extract_subdomains(filename: str) -> List[str]:
+    """Extract subdomains from URLs in a file using regex."""
+    subdomain_list: List[str] = []
+    domain_regex = r'\b(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}\b'
+
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+
+            # Find all matches of domain_regex in the line
+            matches = re.findall(domain_regex, line)
+            for match in matches:
+                subdomain_list.append(match)
+
+    return subdomain_list
+
 
 def save_targets_to_file(domains: List[str], output_file: str) -> None:
     """Save domains to a file."""
---
+++ tests/test_extract_subdomains.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest.mock import mock_open, patch
+from core.targets import extract_subdomains  # Assuming extract_subdomains is implemented in a separate module
+
+class TestExtractSubdomains(unittest.TestCase):
+
+    def test_extract_subdomains(self):
+        file_content = """
+        https://subdomain1.example.com
+        ftp://subdomain2.example.net
+        invalidurl
+        subdomain3.test.wow.com
+        test.com
+        """
+
+        expected_subdomains = ['subdomain1.example.com', 'subdomain2.example.net', 'subdomain3.test.wow.com','test.com']
+
+        # Mocking the file reading
+        with patch('builtins.open', mock_open(read_data=file_content)):
+            actual_subdomains = extract_subdomains('mocked_filename.txt')
+
+        self.assertEqual(actual_subdomains, expected_subdomains)
+
+if __name__ == '__main__':
+    unittest.main()
+
